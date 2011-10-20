ad338cb7eb1e9a9664fb157940fe6e445861f415
trongthanh <trongthanh@gmail.com>
2011-10-20 16:42:16 +0800

    working on in2 panorama

--- projects/In2WebGLPanorama/js/in2data.js
+++ projects/In2WebGLPanorama/js/in2data.js
@@ -3,17 +3,47 @@
  * Date: 10/18/11
  * Time: 3:06 PM
  */
-var officeData = { "panorama": [
+var officeData = { 'panorama': [
   { //1st panorama
-    "mapFile": "img/in2_pano.png",
-    "markers": [
-      {
-        "name": "Jane Koh",
-        "role": "Copywriter",
-        "x": -143.49673797349618, 
-        "y": -97.18085562192277, 
-        "z": 490.94395353515745,
-        "image": "img/people/jane_koh.jpg"
+    'mapFile': 'textures/in2_pano.jpg',
+    'markers': [
+      { 'name': 'Jane Koh',
+        'role': 'Copywriter',
+        'x': -144.26178473196032,
+        'y': -97.4847535699491,
+        'z': 491.08645148975273,
+        'image': 'img/people/jane_koh.jpg'
+      },
+      { 'name': 'Jin Cheung',
+        'role': 'Designer',
+//        'x': 287.5868388148618,
+//        'y': -129.52559486279733,
+//        'z': 398.34634594401905,
+        x: 339.80780029936346,
+y: -165.561484014872,
+z: 319.04960044220866,
+        'image': 'img/people/jin_cheung.jpg'
+      },
+      { 'name': 'Brandon Lee',
+        'role': 'Art Director',
+        'x': 261.28291502641264,
+        'y': -117.40601912530519,
+        'z': 385.33437846553375,
+        'image': 'img/people/brandon_lee.jpg'
+      },
+      { 'name': 'Thanh Tran',
+        'role': 'Web Developer',
+        'x': -86.3953449819011,
+        'y': -112.11062888255337,
+        'z': 490.0836997068697,
+        'image': 'img/people/thanh_tran.jpg'
+      },
+      { 'name': 'Justin Bui Tran',
+        'role': 'Senior Designer',
+        'x': -2.47729003305631,
+        'y': -104.86474210628994,
+        'z': 488.6734803502833,
+        'image': 'img/people/justin_buitran.jpg'
       }
     
     ]
